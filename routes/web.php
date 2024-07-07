8f548e16ae7cfd1ec891ce4e19333e50fb18be34
darshan45672 <drshnbhandary@gmail.com>
2024-07-07 12:28:12 +0530

    added artisan call

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    Artisan::call('migrate');
+
     return view('welcome');
 });
